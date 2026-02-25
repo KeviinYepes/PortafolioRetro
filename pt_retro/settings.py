daeeb1dd398ee7b4a6171b1ad4cc7f26bd02e15c
KeviinYepes <keyv2412@gmail.com>
2026-02-25 12:45:19 -0500

    Django 2.6v

--- pt_retro/settings.py
+++ pt_retro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3l8x-bjbfysr=0@x5i-t9!ag+6)tb!9g1vfr+%*pt=zw7q%otw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
